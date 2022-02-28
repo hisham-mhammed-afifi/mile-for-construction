323520e811d0697d3481a3337e450e2cc530c28b
AquaApple <yassershehab9@gmail.com>
2022-02-28 22:59:05 +0200

    Added Cost model & Controller

---
+++ controllers/cost.controller.js
@@ -0,0 +1,49 @@
+const Cost = require("../models/Cost");
+
+const addCost = async (req, res) => {
+  try {
+    const cost = await Cost.create(req.body);
+    res.status(201).send(cost);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+};
+
+const getAllCost = async (req, res) => {
+  try {
+    const cost = await Cost.find({});
+    res.status(201).send(cost);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+};
+
+const getSingleCost = async (req, res) => {
+  try {
+    const cost = await Cost.findOne({ _id: req.params.id });
+    if (!cost) {
+      res.status(404).send("Cost Not Found");
+    }
+    res.status(200).send(cost);
+  } catch (error) {}
+};
+
+const updateCost = async (req, res) => {
+  try {
+    const cost = await Cost.create();
+    res.status(201).send(cost);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+};
+
+const deleteCost = async (req, res) => {
+  try {
+    const cost = await Cost.create(req.body);
+    res.status(201).send(cost);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+};
+
+module.exports = { addCost, getSingleCost, getAllCost };
