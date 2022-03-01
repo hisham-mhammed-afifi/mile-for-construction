88423dcd16f32838abf32b7cc40ce79de845073a
HishamAfifi <hish.abdelshafouk@gmail.com>
2022-03-01 13:35:54 -1000

    refactor some controllers

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -1,56 +1,38 @@
+const { StatusCodes } = require("http-status-codes");
+const { NotFoundError } = require("../errors");
 const Account = require("../models/Account");
 const Worker = require("../models/Worker");
 
 const addAccount = async (req, res) => {
-  try {
-    const account = await Account.create(req.body);
-    res.status(201).send(account);
-  } catch (err) {
-    res.status(400).send(err.message);
-  }
+  const account = await Account.create(req.body);
+  res.status(StatusCodes.CREATED).json({ account });
 };
 
 const getAllAccounts = async (req, res) => {
-  try {
-    const account = await Account.find({});
-    res.status(200).send(account);
-  } catch (err) {
-    res.status(404).send(err.message);
-  }
+  const account = await Account.find({});
+  res.status(StatusCodes.OK).json({ account });
 };
 const getAccount = async (req, res) => {
-  try {
-    const account = await Account.find({ _id: req.params.id });
-    console.log(req.params.id);
-    if (!account) {
-      return res.status(404).send("account Not Found");
-    }
-    res.status(200).send(account);
-  } catch (err) {
-    res.status(404).send(err.message);
+  const account = await Account.find({ _id: req.params.id });
+  console.log(req.params.id);
+  if (!account) {
+    throw new NotFoundError("Account not found");
   }
+  res.status(StatusCodes.OK).json({ account });
 };
 const updateAccount = async (req, res) => {
-  try {
-    const account = await Worker.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(200).send(worker);
-  } catch (error) {
-    res.status(404).send(error.message);
-  }
+  const account = await Account.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(StatusCodes.OK).json({ account });
 };
 const deleteAccount = async (req, res) => {
-  try {
-    const account = await Worker.findByIdAndDelete(id);
-    if (!account) {
-      res.status(404).send("account Not Found");
-    }
-    res.status(200).send("Sucssefully Deleted");
-  } catch (error) {
-    res.status(400).send(error.message);
+  const account = await Account.findByIdAndDelete(id);
+  if (!account) {
+    throw new NotFoundError("Account not found");
   }
+  res.status(StatusCodes.OK).json({ msg: "Sucssefully Deleted" });
 };
 module.exports = {
   addAccount,
